d749304c7298a14b7e927c189ea2d19a61a1dcad
Alena Nahornaya <nahornaya.alena@gmail.com>
2024-01-05 17:53:36 +0100

    adding new task function added

--- js/script.js
+++ js/script.js
@@ -20,12 +20,35 @@
             </li>
             `;
         }
-
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+            done: false,
+        });
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-form__input").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
     };
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
